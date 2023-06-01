44521fe440330590b6f9bb524ced423deb874969
Zeox4 <59748901+Zeox4@users.noreply.github.com>
2023-06-01 12:23:22 -0600

    cambio en paguina
    se realizo cambios en nombre de archivos y el que era index ya no lo es, ya funciona el fondo con video

---
+++ Lovely-Stalking 2/Pagina lokona/script/cript.js
@@ -0,0 +1,90 @@
+// Get the modals
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
+
+// Get the buttons that open the modals
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+
+// Get the <span> elements that close the modals
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
+var span5 = document.getElementsByClassName("close")[4];
+var span6 = document.getElementsByClassName("close")[5];
+
+// When the user clicks the button, open the corresponding modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the corresponding modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close the corresponding modal
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+  } else if (event.target == modal3) {
+    modal3.style.display = "none";
+  } else if (event.target == modal4) {
+    modal4.style.display = "none";
+  } else if (event.target == modal5) {
+    modal5.style.display = "none";
+  } else if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(function(stream) {
+    var video = document.getElementById('webcam');
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(function(error) {
+    console.log('Error accessing the webcam:', error);
+  });
